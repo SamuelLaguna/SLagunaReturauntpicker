52201c248331cca5dabcac67bb812eb9fd215f4b
SamuelLaguna <samanater11@gmail.com>
2022-10-18 15:50:56 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿//Samuel Laguna 
+//10-18-22
+//We are making a program called returaunt picker. We are to have 3 catrigorys of resturaunts and then have ten option in each catigory.
+//Peer reivew: 
+Console.Clear();
+
+
+ 
+ string[]PizzaArray = {"Davids Pizza","Eddies Pizza","Micheles Pizza","Dominos","Little Ceazears","Strawhat","PizzaHut","PizzaGuys","RoundTable","Papa Johns"};
+ string[]BurgerArray = {"In n Out","Habit","Bj's","Chilies","Applebees","McDonolds","Burger king","Sqeez burger","Red Robin","Famoues Burger & Brew"};
+ string[]ChickenArray = {"KFC","Rasin Canes","Chick fil a","Churches Chicken","Luus Chicken bowl","Popyes","WingStop","Bufflo Wild Wings","Fire Wings","Rays Chicken"};
+
+ Console.WriteLine(PizzaArray.Length);
+ Console.WriteLine(PizzaArray[0]);
+
+
+Console.WriteLine(ChickenArray.Length);
+Console.WriteLine(ChickenArray[4]);
+
+
+Console.WriteLine(BurgerArray.Length);
+Console.WriteLine(BurgerArray[7]);
